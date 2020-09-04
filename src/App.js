1c706e746301f5317a762b4ccd0669e3decdd20f
Dananjaya94 <nuwan_windows7@live.com>
2020-09-04 08:33:00 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router , Route , Link } from "react-router-dom";
 import './App.css';
+import { Route } from 'react-router-dom';
+
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div className = "container">
       <h2>Customer Service Desk</h2>
+
+      <Route path = "/" exact  component = {Login}></Route>
+      <Route path = "/home" exact  component = {Dashboard}></Route>
+      <Route path = "/viponthespot" exact  component = {Login}></Route>
+      
     </div>
   );
 }
