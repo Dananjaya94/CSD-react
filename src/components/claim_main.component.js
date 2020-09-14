b79662be16e4bba8987131d30d66e10a4552a7e2
Dananjaya94 <nuwan_windows7@live.com>
2020-09-14 15:29:26 +0530

    Update claim_main.component.js
    updated

--- src/components/claim_main.component.js
+++ src/components/claim_main.component.js
@@ -1,11 +1,67 @@
 import React, {Component} from 'react';
 import $ from 'jquery';
 
+var outcolls = [];
+var outrowsss = [];
+
 $(document).ready(function (){
-    alert(JSON.parse(localStorage.getItem('selectednumber')));
-    alert(JSON.parse(localStorage.getItem('selectedpolicynumber')));
+    // alert(JSON.parse(localStorage.getItem('selectednumber')));
+    // alert(JSON.parse(localStorage.getItem('selectedpolicynumber')));
     console.log(JSON.parse(localStorage.getItem('selectednumber')));
     console.log(JSON.parse(localStorage.getItem('selectedpolicynumber')));
+
+    var vh_data = {};
+    vh_data.str = JSON.parse(localStorage.getItem('entrval'));
+    vh_data.usr = JSON.parse(localStorage.getItem('user'));
+    vh_data.pwd = JSON.parse(localStorage.getItem('pass'));
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:4000/GetAsset/:id",
+        data: {
+            str : vh_data
+        } ,
+        contentType: "application/json",
+        beforeSend: function () {
+
+        },
+        success: function (data) {
+            $.each(data, function (index, value) {
+                outcolls = [];
+                outrowsss= [];
+
+                console.log(value);
+                
+                // for (var o in value.metaData)
+                // {
+                //     outcolls.push(value.metaData[o]);
+                // }
+                // for (var i in value.rows)
+                // {
+                //     outrowsss.push(value.rows[i]);
+                // }
+                // console.log(outrowsss);
+                
+                // $('#searchresulttable').DataTable({
+                //     data: outrowsss,
+                //     columns: 
+                //     [
+                //         { title: "VEHICLE NUMBER" },
+                //         { title: "POLICY NUMBER" },
+                //         { title: "NAME OF THE INSURED" },
+                //         { title: "BRANCH" },
+                //         { title: "POLICY START" },
+                //         { title: "POLICY END" },
+                //         { title: "SUM INSURED"},
+                //     ]
+                // });
+            })
+        },
+        error: function (jqXHR, exception) {
+
+        }
+    
+    });
 });
 
 export default class ClaimMain extends Component
